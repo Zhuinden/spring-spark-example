1d904d66e8587410f20263d5c4a1cb737e587bd8
zhuinden <zhuinden@gmail.com>
2017-03-29 15:01:57 +0200

    .

--- src/test/java/com/zhuinden/sparkexperiment/SparkExperimentApplicationTests.java
+++ src/test/java/com/zhuinden/sparkexperiment/SparkExperimentApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SparkexperimentApplicationTests {
+public class SparkExperimentApplicationTests {
 
 	@Test
 	public void contextLoads() {
